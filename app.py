5e27159c7d6e8f65b0f4c96985fc08c49f428a55
jayp936 <jayppatel3627@gmail.com>
2024-05-28 12:48:53 -0400

    'deleted'

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
 
-
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
@@ -39,6 +38,5 @@ def predict_datapoint():
     
     
 
-if __name__=="__main__":
-    # app.run(host="0.0.0.0",port=8080)        
-    app.run(host='0.0.0.0', port=8080)        
+if __name__=="__main__":      
+    app.run(host="0.0.0.0",port=80)        
